e2e18fc4d4b0f6b0b26112854b35bf931d8860e4
Christopher Moore <christopher.e.moore@gmail.com>
2023-06-12 09:51:38 +0800

    switching to modules

--- main.js
+++ main.js
@@ -1,39 +1,59 @@
-var canvas;
-var gl;
-var glutil;
+import {mat4} from '/js/gl-matrix-3.4.1/index.js';
+import {DOM, getIDs, removeFromParent} from '/js/util.js';
+import {GLUtil} from '/js/gl-util.js';
+import {Mouse3D} from '/js/mouse3d.js';
 
-var modulo = 4;
-var initValue = 1<<16;
-var drawValue = 25;
-var gridsize = +$.url().param('size');
-if (gridsize !== gridsize) gridsize = 1024;
+//shitty new system because how do you call import() blocking
+import {makePingPong} from '/js/gl-util-PingPong.js';
+import {makeUnitQuad} from '/js/gl-util-UnitQuad.js';
 
-var inputMethod = 'pan';
+const ids = getIDs();
+window.ids = ids;
 
-var bufferCPU = new Uint8Array(4 * gridsize * gridsize);
+const urlparams = new URLSearchParams(location.search);
 
-var colors = [
+let canvas;
+let gl;
+let glutil;
+let mouse;
+let pingpong;
+let grad;
+let updateShader, displayShader;
+
+const _G = {};
+
+let modulo = 4;
+_G.initValue = 1<<16;
+_G.drawValue = 25;
+let gridsize = +urlparams.get('size');
+if (!gridsize || !isFinite(gridsize)) gridsize = 1024;
+
+let inputMethod = document.querySelector('input[name="inputMethod"]:checked').value;
+
+let bufferCPU = new Uint8Array(4 * gridsize * gridsize);
+
+let colors = [
 	[0,0,0],
 	[0,255,255],
 	[255,255,0],
 	[255,0,0]
 ];
 
-var totalSand = 0;
+let totalSand = 0;
 	
 function setTotalSand(newTotalSand) {
 	totalSand = newTotalSand;
-	$('#totalSand').text(''+totalSand);
+	ids.totalSand.innerText = ''+totalSand;
 }
 
 function reset() {
 	bufferCPU.fill();
-	bufferCPU[0 + 4 * ((gridsize>>1) + gridsize * (gridsize>>1))] = initValue & 0xff;
-	bufferCPU[1 + 4 * ((gridsize>>1) + gridsize * (gridsize>>1))] = (initValue >> 8) & 0xff;
-	bufferCPU[2 + 4 * ((gridsize>>1) + gridsize * (gridsize>>1))] = (initValue >> 16) & 0xff;
-	bufferCPU[3 + 4 * ((gridsize>>1) + gridsize * (gridsize>>1))] = (initValue >> 24) & 0xff;
+	bufferCPU[0 + 4 * ((gridsize>>1) + gridsize * (gridsize>>1))] = _G.initValue & 0xff;
+	bufferCPU[1 + 4 * ((gridsize>>1) + gridsize * (gridsize>>1))] = (_G.initValue >> 8) & 0xff;
+	bufferCPU[2 + 4 * ((gridsize>>1) + gridsize * (gridsize>>1))] = (_G.initValue >> 16) & 0xff;
+	bufferCPU[3 + 4 * ((gridsize>>1) + gridsize * (gridsize>>1))] = (_G.initValue >> 24) & 0xff;
 
-	$.each(pingpong.history, function(i,h) {
+	pingpong.history.forEach(h => {
 		h.bind();
 		//will the pointer type cast be allowed?
 		//or will I have to reshape the int32 data into uint8 x4 data?
@@ -41,8 +61,12 @@ function reset() {
 		h.unbind();
 	});
 
-	setTotalSand(initValue);
+	setTotalSand(_G.initValue);
 }
+//can't declare _G.<function> if _G is decared const, because javascript ...
+// even tho const means _G's ref is immutable, but not _G's contents .... 
+// smh javascript
+_G.reset = reset;
 
 function initGL() {
 	gl.clearColor(.2, .2, .2, 1);
@@ -62,7 +86,7 @@ function initGL() {
 	});
 	pingpong.fbo.bind();
 	pingpong.fbo.unbind();
-	reset();
+	_G.reset();
 
 	grad = new glutil.Texture2D({
 		width : modulo,
@@ -80,30 +104,27 @@ function initGL() {
 	});
 	grad.bind(1);
 
-	var glstr = function(x) {
-		var s = ''+x;
+	let glstr = function(x) {
+		let s = ''+x;
 		if (s.indexOf('.') == -1) s += '.';
 		return s;
 	};
 
-	updateShader = new glutil.ShaderProgram({
-		vertexPrecision : 'best',
-		vertexCode : mlstr(function(){/*
-attribute vec2 vertex;
+	updateShader = new glutil.Program({
+		vertexCode : `
+in vec2 vertex;
 uniform mat4 projMat, mvMat;
-varying vec2 tc;
+out vec2 tc;
 void main() {
 	tc = vertex.st;
 	gl_Position = projMat * mvMat * vec4(vertex, 0., 1.);
 }
-*/}),
-		fragmentPrecision : 'best',
-		fragmentCode : 
-		
-'const float du = '+glstr(1/gridsize)+';\n'+
-'const float modulo = '+glstr(modulo)+';\n'+
-mlstr(function(){/*
-varying vec2 tc;
+`,
+		fragmentCode : `
+const float du = `+glstr(1/gridsize)+`;
+const float modulo = `+glstr(modulo)+`;
+
+in vec2 tc;
 uniform sampler2D tex;
 
 //divide by modulo
@@ -115,14 +136,15 @@ vec4 fixedShift(vec4 v) {
 	return v;
 }
 
+out vec4 fragColor;
 void main() {
-	vec4 last = texture2D(tex, tc);
+	vec4 last = texture(tex, tc);
 	
 	//sum neighbors
-	vec4 next = fixedShift(texture2D(tex, tc + vec2(du, 0)))
-		+ fixedShift(texture2D(tex, tc + vec2(-du, 0)))
-		+ fixedShift(texture2D(tex, tc + vec2(0, du)))
-		+ fixedShift(texture2D(tex, tc + vec2(0, -du)));
+	vec4 next = fixedShift(texture(tex, tc + vec2(du, 0)))
+		+ fixedShift(texture(tex, tc + vec2(-du, 0)))
+		+ fixedShift(texture(tex, tc + vec2(0, du)))
+		+ fixedShift(texture(tex, tc + vec2(0, -du)));
 
 	//add last cell modulo
 	next.r += mod(last.r, modulo / 256.);
@@ -132,37 +154,35 @@ void main() {
 	next.b += floor(next.g) / 256.;
 	next.a += floor(next.b) / 256.;
 	next = mod(next, 1.);
-	gl_FragColor = next;
+	fragColor = next;
 }
-*/}),
+`,
 		uniforms : {
 			tex : 0
 		}
 	});
 
-	displayShader = new glutil.ShaderProgram({
-		vertexPrecision : 'best',
-		vertexCode : mlstr(function(){/*
-attribute vec2 vertex;
+	displayShader = new glutil.Program({
+		vertexCode : `
+in vec2 vertex;
 uniform mat4 projMat, mvMat;
-varying vec2 tc;
+out vec2 tc;
 void main() {
 	tc = vertex;
 	gl_Position = projMat * mvMat * vec4(vertex, 0., 1.);
 }
-*/}),
-		fragmentPrecision : 'best',
-		fragmentCode : 
-'const float modulo = '+modulo+'.;\n'
-+mlstr(function(){/*
-varying vec2 tc;
+`,
+		fragmentCode : `
+const float modulo = `+modulo+`.;
+in vec2 tc;
 uniform sampler2D tex, grad;
+out vec4 fragColor;
 void main() {
-	vec3 toppleColor = texture2D(tex, tc).rgb;
+	vec3 toppleColor = texture(tex, tc).rgb;
 	float value = toppleColor.r * (256. / modulo);
-	gl_FragColor = texture2D(grad, vec2(value + (.5 / modulo), .5));
+	fragColor = texture(grad, vec2(value + (.5 / modulo), .5));
 }
-*/}),
+`,
 		uniforms : {
 			tex : 0,
 			grad : 1
@@ -170,40 +190,40 @@ void main() {
 	});
 }
 
-var lastX = undefined;
-var lastY = undefined;
+let lastX = undefined;
+let lastY = undefined;
 function update() {
 	glutil.draw();
 
 	//TODO just draw a 1x1 quad over the correct pixel
 	if (inputMethod == 'draw' && mouse.isDown) {
-		var ar = canvas.width / canvas.height;
-		var thisX = (mouse.xf - .5) * 2 * glutil.view.fovY * ar + glutil.view.pos[0];
-		var thisY = (1 - mouse.yf - .5) * 2 * glutil.view.fovY + glutil.view.pos[1];
+		let ar = canvas.width / canvas.height;
+		let thisX = (mouse.xf - .5) * 2 * glutil.view.fovY * ar + glutil.view.pos[0];
+		let thisY = (1 - mouse.yf - .5) * 2 * glutil.view.fovY + glutil.view.pos[1];
 		thisX = Math.floor(thisX * gridsize + .5);
 		thisY = Math.floor(thisY * gridsize + .5);
 		if (lastX === undefined) lastX = thisX;
 		if (lastY === undefined) lastY = thisY;
 
-		var dx = thisX - lastX;
-		var dy = thisY - lastY;
-		var d = Math.ceil(Math.max(Math.abs(dx), Math.abs(dy), 1));
+		let dx = thisX - lastX;
+		let dy = thisY - lastY;
+		let d = Math.ceil(Math.max(Math.abs(dx), Math.abs(dy), 1));
 
-		for (var i = .5; i <= d; ++i) {
-			var f = i / d;
-			var _f = 1 - f;
-			var x = _f * thisX + f * lastX;
-			var y = _f * thisY + f * lastY;
+		for (let i = .5; i <= d; ++i) {
+			let f = i / d;
+			let _f = 1 - f;
+			let x = _f * thisX + f * lastX;
+			let y = _f * thisY + f * lastY;
 		
 			if (x >= 0 && x < gridsize && y >= 0 && y < gridsize) {
-				var value = new Uint8Array(4);
+				let value = new Uint8Array(4);
 				pingpong.draw({
 					callback : function() {
 						gl.readPixels(x, y, 1, 1, gl.RGBA, gl.UNSIGNED_BYTE, value);
 					}
 				});
-				var intvalue = value[0] | (value[1] << 8) | (value[2] << 16) | (value[3] << 24);
-				intvalue += drawValue;
+				let intvalue = value[0] | (value[1] << 8) | (value[2] << 16) | (value[3] << 24);
+				intvalue += _G.drawValue;
 				value[0] = intvalue & 0xff;
 				value[1] = (intvalue >> 8) & 0xff;
 				value[2] = (intvalue >> 16) & 0xff;
@@ -212,7 +232,7 @@ function update() {
 				pingpong.current().bind();
 				gl.texSubImage2D(gl.TEXTURE_2D, 0, x, y, 1, 1, gl.RGBA, gl.UNSIGNED_BYTE, value)
 				pingpong.current().unbind();
-				setTotalSand(totalSand + drawValue);
+				setTotalSand(totalSand + _G.drawValue);
 			}
 		}
 
@@ -220,17 +240,17 @@ function update() {
 		lastY = thisY;
 	}
 
-	var fboProjMat = mat4.create();
+	let fboProjMat = mat4.create();
 	mat4.identity(fboProjMat);
 	mat4.ortho(fboProjMat, 0, 1, 0, 1, -1, 1);
-	var fboMvMat = mat4.create();
+	let fboMvMat = mat4.create();
 	mat4.identity(fboMvMat);
 
 	pingpong.swap();
 	pingpong.draw({
 		viewport : [0,0,gridsize,gridsize],
 		callback : function() {
-			glutil.unitQuad.draw({
+			glutil.UnitQuad.unitQuad.draw({
 				shader : updateShader,
 				texs : [pingpong.previous()],
 				uniforms : {
@@ -241,109 +261,107 @@ function update() {
 		},
 	});
 	
-	glutil.unitQuad.draw({
+	glutil.UnitQuad.unitQuad.draw({
 		shader : displayShader,
 		texs : [pingpong.current()]
 	});
 
-	//requestAnimFrame(update);
-	setTimeout(update, 0);
+	requestAnimationFrame(update);
+	//setTimeout(update, 0);
 }
 
-$(document).ready(function(){
-	canvas = $('<canvas>', {
-		css : {
-			left : 0,
-			top : 0,
-			position : 'absolute'
-		}
-	}).prependTo(document.body).get(0);
-	$(canvas).disableSelection()
-
-	try {
-		glutil = new GLUtil({
-			canvas : canvas,
-			fullscreen : true
-		});
-		gl = glutil.context;
-	} catch (e) {
-		$(canvas).remove(); throw e;
-	}
-
-	mouse = new Mouse3D({
-		pressObj : canvas,
-		move : function(dx,dy) {
-			if (inputMethod == 'pan') {
-				glutil.view.pos[0] -= dx / canvas.height * 2 * glutil.view.fovY;
-				glutil.view.pos[1] += dy / canvas.height * 2 * glutil.view.fovY;
-				glutil.updateProjection();
-			} 
-		},
-		zoom : function(dz) {
-			glutil.view.fovY *= Math.exp(-.1 * dz / canvas.height);
-			glutil.updateProjection();
-		},
-		mousedown : function() {
-			lastX = undefined;
-			lastY = undefined;
-		}
-	});
-
-	glutil.view.ortho = true;
-	glutil.view.zNear = -1;
-	glutil.view.zFar = 1;
-	glutil.view.fovY = .5;
-	glutil.view.pos[0] = .5;
-	glutil.view.pos[1] = .5;
-	glutil.updateProjection();
-
-	var maxsize =  gl.getParameter(gl.MAX_TEXTURE_SIZE);
-	if (gridsize > maxsize) gridsize = maxsize;
-	var gridsizes = $('#gridsize');
-	for (var size = 32; size <= maxsize; size<<=1) {
-		var option = $('<option>', {
-			text : size,
-			value : size
-		});
-		if (size == gridsize) option.attr('selected', 'true');
-		gridsizes.append(option);
-	}
-	gridsizes.change(function() {
-		var params = $.url().param();
-		params.size = gridsizes.val();
-		var url = location.href.match('[^?]*')[0];
-		var sep = '?';
-		for (k in params) {
-			if (k != '') {
-				url += sep;
-				url += k + '=' + params[k];
-				sep = '&';
-			}
-		}
-		location.href = url;
-	});
-
-	$.each(['reset'], function(i, field) {
-		$('#'+field).click(function() {
-			window[field]();
-		});
-	});
-
-	$.each(['initValue', 'drawValue'], function(i, field) {
-		$('#'+field)
-			.val(''+window[field])
-			.change(function() {
-				window[field] = +$(this).val();
-			});
-	});
-
-	//https://stackoverflow.com/questions/4618733/set-selected-radio-from-radio-group-with-a-value#4618748
-	var updateRadio = function() { $('input[name=inputMethod]').val([inputMethod]); };
-	$('#inputMethod_pan').click(function() { inputMethod = 'pan'; });
-	$('#inputMethod_draw').click(function() { inputMethod = 'draw'; });
-	$('#button_pan').click(function() { inputMethod = 'pan'; updateRadio(); });
-	$('#button_draw').click(function() { inputMethod = 'draw'; updateRadio(); });
-
-	initGL();
-	update();
+canvas = DOM('canvas', {
+	css : {
+		left : 0,
+		top : 0,
+		position : 'absolute',
+		userSelect : 'none',
+	},
+	prependTo : document.body,
 });
+
+try {
+	glutil = new GLUtil({
+		canvas : canvas,
+		fullscreen : true
+	});
+	gl = glutil.context;
+} catch (e) {
+	removeFromParent(canvas);
+	throw e;
+}
+glutil.import('PingPong', makePingPong);
+glutil.import('UnitQuad', makeUnitQuad);
+
+mouse = new Mouse3D({
+	pressObj : canvas,
+	move : function(dx,dy) {
+		if (inputMethod == 'pan') {
+			glutil.view.pos[0] -= dx / canvas.height * 2 * glutil.view.fovY;
+			glutil.view.pos[1] += dy / canvas.height * 2 * glutil.view.fovY;
+			glutil.updateProjection();
+		} 
+	},
+	zoom : function(dz) {
+		glutil.view.fovY *= Math.exp(-.1 * dz / canvas.height);
+		glutil.updateProjection();
+	},
+	mousedown : function() {
+		lastX = undefined;
+		lastY = undefined;
+	}
+});
+
+glutil.view.ortho = true;
+glutil.view.zNear = -1;
+glutil.view.zFar = 1;
+glutil.view.fovY = .5;
+glutil.view.pos[0] = .5;
+glutil.view.pos[1] = .5;
+glutil.updateProjection();
+
+let maxsize =  gl.getParameter(gl.MAX_TEXTURE_SIZE);
+if (gridsize > maxsize) gridsize = maxsize;
+for (let size = 32; size <= maxsize; size<<=1) {
+	let option = DOM('option', {
+		text : size,
+		value : size,
+		appendTo : ids.gridsize,
+	});
+	if (size == gridsize) option.setAttribute('selected', 'true');
+}
+ids.gridsize.addEventListener('change', e => {
+	const params = new URLSearchParams(urlparams);
+	params.set('size', ids.gridsize.value);
+	location.href = location.origin + location.pathname + '?' + params.toString();
+});
+
+['reset'].forEach(field => {
+	ids[field].addEventListener('click', e => {
+		_G[field]();
+	});
+});
+
+['initValue', 'drawValue'].forEach(field => {
+	const o = ids[field];
+	o.value = _G[field];
+	o.addEventListener('change', e => {
+		_G[field] = +o.value;
+	});
+});
+
+// TODO here and conway-life-webgl a better way ...
+let updateRadio = function() {
+	for (let k in ids) {
+		if (k.substr(0,11) == 'inputMethod') {
+			ids[k].checked = ids[k].value == inputMethod;
+		}
+	}
+};
+ids.inputMethod_pan.addEventListener('click', e => { inputMethod = 'pan'; });
+ids.inputMethod_draw.addEventListener('click', e => { inputMethod = 'draw'; });
+ids.button_pan.addEventListener('click', e => { inputMethod = 'pan'; updateRadio(); });
+ids.button_draw.addEventListener('click', e => { inputMethod = 'draw'; updateRadio(); });
+
+initGL();
+update();
